af46a713d82b354ff6f12c62fc3b4667fcdc13b1
Evan You <yyx990803@gmail.com>
2016-08-08 23:50:28 -0400

    tweak server

---
+++ build/setup-dev-server.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const webpack = require('webpack')
+const MFS = require('memory-fs')
+const clientConfig = require('./webpack.client.config')
+const serverConfig = require('./webpack.server.config')
+
+module.exports = function setupDevServer (app, onUpdate) {
+  // setup on the fly compilation + hot-reload
+  clientConfig.entry.app = ['webpack-hot-middleware/client', clientConfig.entry.app]
+  clientConfig.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  )
+
+  const clientCompiler = webpack(clientConfig)
+  app.use(require('webpack-dev-middleware')(clientCompiler, {
+    publicPath: clientConfig.output.publicPath,
+    stats: {
+      colors: true,
+      chunks: false
+    }
+  }))
+  app.use(require('webpack-hot-middleware')(clientCompiler))
+
+  // watch and update server renderer
+  const serverCompiler = webpack(serverConfig)
+  const mfs = new MFS()
+  const outputPath = path.join(serverConfig.output.path, serverConfig.output.filename)
+  serverCompiler.outputFileSystem = mfs
+  serverCompiler.watch({}, (err, stats) => {
+    if (err) throw err
+    stats = stats.toJson()
+    stats.errors.forEach(err => console.error(err))
+    stats.warnings.forEach(err => console.warn(err))
+    onUpdate(mfs.readFileSync(outputPath, 'utf-8'))
+  })
+}
--- server.js
+++ server.js
@@ -7,54 +7,35 @@ const favicon = require('serve-favicon')
 const serialize = require('serialize-javascript')
 const createBundleRenderer = require('vue-server-renderer').createBundleRenderer
 
-let renderer
-function createRenderer (fs) {
-  const bundlePath = path.resolve(__dirname, 'dist/server-bundle.js')
-  return createBundleRenderer(fs.readFileSync(bundlePath, 'utf-8'))
-}
-
 const app = express()
 
+// parse index.html template
+const html = (() => {
+  const template = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8')
+  const i = template.indexOf('{{ APP }}')
+  const style = process.env.NODE_ENV === 'production'
+    ? '<link rel="stylesheet" href="/dist/styles.css">'
+    : ''
+  return {
+    head: template.slice(0, i).replace('{{ STYLE }}', style),
+    tail: template.slice(i + '{{ APP }}'.length)
+  }
+})()
+
+// setup the server renderer, depending on dev/prod environment
+let renderer
 if (process.env.NODE_ENV !== 'production') {
-  const webpack = require('webpack')
-  const clientConfig = require('./build/webpack.client.config')
-
-  clientConfig.entry.app = ['webpack-hot-middleware/client', clientConfig.entry.app]
-  clientConfig.plugins.push(
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  )
-
-  const clientCompiler = webpack(clientConfig)
-  app.use(require('webpack-dev-middleware')(clientCompiler, {
-    publicPath: clientConfig.output.publicPath,
-    stats: {
-      colors: true,
-      chunks: false
-    }
-  }))
-  app.use(require('webpack-hot-middleware')(clientCompiler))
-
-  // watch and update server renderer
-  const MFS = require('memory-fs')
-  const serverConfig = require('./build/webpack.server.config')
-  const serverCompiler = webpack(serverConfig)
-  const mfs = new MFS()
-  serverCompiler.outputFileSystem = mfs
-  serverCompiler.watch({}, (err, stats) => {
-    if (err) throw err
-    stats = stats.toJson()
-    stats.errors.forEach(err => console.error(err))
-    stats.warnings.forEach(err => console.warn(err))
-    renderer = createRenderer(mfs)
+  require('./build/setup-dev-server')(app, bundle => {
+    renderer = createBundleRenderer(bundle)
   })
 } else {
-  app.use('/dist', express.static(path.resolve(__dirname, 'dist')))
   // create server renderer from real fs
-  renderer = createRenderer(fs)
+  const bundlePath = path.resolve(__dirname, './dist/server-bundle.js')
+  renderer = createBundleRenderer(fs.readFileSync(bundlePath, 'utf-8'))
 }
 
-app.use(favicon(path.resolve(__dirname, 'src/assets/logo.png')))
+app.use('/dist', express.static(path.resolve(__dirname, './dist')))
+app.use(favicon(path.resolve(__dirname, './src/assets/logo.png')))
 
 app.get('*', (req, res) => {
   var s = Date.now()
@@ -62,25 +43,17 @@ app.get('*', (req, res) => {
   const renderStream = renderer.renderToStream(context)
   let firstChunk = true
 
-  res.write(`<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>vue-hackernews-2.0</title>
-    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
-    ${process.env.NODE_ENV === 'production'
-      ? `<link rel="stylesheet" href="/dist/styles.css">`
-      : ``}
-  </head>
-  <body>`)
+  res.write(html.head)
 
   renderStream.on('data', chunk => {
     if (firstChunk) {
-      // send down initial store state
+      // embed initial store state
       if (context.initialState) {
-        res.write(`<script>window.__INITIAL_STATE__=${
-          serialize(context.initialState, { isJSON: true })
-        }</script>`)
+        res.write(
+          `<script>window.__INITIAL_STATE__=${
+            serialize(context.initialState, { isJSON: true })
+          }</script>`
+        )
       }
       firstChunk = false
     }
@@ -88,11 +61,7 @@ app.get('*', (req, res) => {
   })
 
   renderStream.on('end', () => {
-    res.end(`
-      <script src="/dist/client-vendor-bundle.js"></script>
-      <script src="/dist/client-bundle.js"></script>
-      </body></html>`
-    )
+    res.end(html.tail)
     console.log(`whole request: ${Date.now() - s}ms`)
   })
 
