96b8dafc5c4bb13b51023f20e6264abbdf0f71d9
Valentin Vieriu <vvieriu@sapient.com>
2016-09-12 04:35:01 +0200

    Enabling cluster mode

--- server.js
+++ server.js
@@ -1,92 +1,114 @@
 process.env.VUE_ENV = 'server'
 const isProd = process.env.NODE_ENV === 'production'
+const cluster = require('cluster');
 
-const fs = require('fs')
-const path = require('path')
-const resolve = file => path.resolve(__dirname, file)
-const express = require('express')
-const favicon = require('serve-favicon')
-const serialize = require('serialize-javascript')
+if (cluster.isMaster) {
+  var numCPUs = require('os').cpus().length;
 
-// https://github.com/vuejs/vue/blob/next/packages/vue-server-renderer/README.md#why-use-bundlerenderer
-const createBundleRenderer = require('vue-server-renderer').createBundleRenderer
-
-const app = express()
-
-// parse index.html template
-const html = (() => {
-  const template = fs.readFileSync(resolve('./index.html'), 'utf-8')
-  const i = template.indexOf('{{ APP }}')
-  // styles are injected dynamically via vue-style-loader in development
-  const style = isProd ? '<link rel="stylesheet" href="/dist/styles.css">' : ''
-  return {
-    head: template.slice(0, i).replace('{{ STYLE }}', style),
-    tail: template.slice(i + '{{ APP }}'.length)
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
-})()
 
-// setup the server renderer, depending on dev/prod environment
-let renderer
-if (isProd) {
-  // create server renderer from real fs
-  const bundlePath = resolve('./dist/server-bundle.js')
-  renderer = createRenderer(fs.readFileSync(bundlePath, 'utf-8'))
-} else {
-  require('./build/setup-dev-server')(app, bundle => {
-    renderer = createRenderer(bundle)
-  })
+  cluster.on('exit', function (worker, code, signal) {
+    console.error(['The worker #' + worker.id + ' has exited with exitCode ' + worker.process.exitCode]);
+    // Don't try to restart the workers when disconnect or destroy has been called
+    if (worker.suicide !== true) {
+      console.log('Worker #' + worker.id + ' did not commit suicide, restarting');
+      cluster.fork();
+    }
+  });
+  cluster.on('disconnect', function (worker) {
+    console.log('The worker #' + worker.id + ' has disconnected');
+  });
 }
+else {
+  const fs = require('fs');
+  const path = require('path');
+  const resolve = file => path.resolve(__dirname, file);
+  const express = require('express');
+  const favicon = require('serve-favicon');
+  const serialize = require('serialize-javascript');
 
-function createRenderer (bundle) {
-  return createBundleRenderer(bundle, {
-    cache: require('lru-cache')({
-      max: 1000,
-      maxAge: 1000 * 60 * 15
+  // https://github.com/vuejs/vue/blob/next/packages/vue-server-renderer/README.md#why-use-bundlerenderer
+  const createBundleRenderer = require('vue-server-renderer').createBundleRenderer
+
+  const app = express()
+
+  // parse index.html template
+  const html = (() => {
+    const template = fs.readFileSync(resolve('./index.html'), 'utf-8')
+    const i = template.indexOf('{{ APP }}')
+    // styles are injected dynamically via vue-style-loader in development
+    const style = isProd ? '<link rel="stylesheet" href="/dist/styles.css">' : ''
+    return {
+      head: template.slice(0, i).replace('{{ STYLE }}', style),
+      tail: template.slice(i + '{{ APP }}'.length)
+    }
+  })()
+
+  // setup the server renderer, depending on dev/prod environment
+  let renderer
+  if (isProd) {
+    // create server renderer from real fs
+    const bundlePath = resolve('./dist/server-bundle.js')
+    renderer = createRenderer(fs.readFileSync(bundlePath, 'utf-8'))
+  } else {
+    require('./build/setup-dev-server')(app, bundle => {
+      renderer = createRenderer(bundle)
+    })
+  }
+
+  function createRenderer(bundle) {
+    return createBundleRenderer(bundle, {
+      cache: require('lru-cache')({
+        max: 1000,
+        maxAge: 1000 * 60 * 15
+      })
+    })
+  }
+
+  app.use('/dist', express.static(resolve('./dist')))
+  app.use(favicon(resolve('./src/assets/logo.png')))
+
+  app.get('*', (req, res) => {
+    if (!renderer) {
+      return res.end('waiting for compilation...')
+    }
+
+    var s = Date.now()
+    const context = { url: req.url }
+    const renderStream = renderer.renderToStream(context)
+    let firstChunk = true
+
+    res.write(html.head)
+
+    renderStream.on('data', chunk => {
+      if (firstChunk) {
+        // embed initial store state
+        if (context.initialState) {
+          res.write(
+            `<script>window.__INITIAL_STATE__=${
+            serialize(context.initialState, { isJSON: true })
+            }</script>`
+          )
+        }
+        firstChunk = false
+      }
+      res.write(chunk)
+    })
+
+    renderStream.on('end', () => {
+      res.end(html.tail)
+      console.log(`whole request: ${Date.now() - s}ms`)
+    })
+
+    renderStream.on('error', err => {
+      throw err
     })
   })
+
+  const port = process.env.PORT || 8080
+  app.listen(port, () => {
+    console.log(`APPLICATION STARTED WORKER=${cluster.worker && cluster.worker.id} PORT=${port}`)
+  });
 }
-
-app.use('/dist', express.static(resolve('./dist')))
-app.use(favicon(resolve('./src/assets/logo.png')))
-
-app.get('*', (req, res) => {
-  if (!renderer) {
-    return res.end('waiting for compilation...')
-  }
-
-  var s = Date.now()
-  const context = { url: req.url }
-  const renderStream = renderer.renderToStream(context)
-  let firstChunk = true
-
-  res.write(html.head)
-
-  renderStream.on('data', chunk => {
-    if (firstChunk) {
-      // embed initial store state
-      if (context.initialState) {
-        res.write(
-          `<script>window.__INITIAL_STATE__=${
-            serialize(context.initialState, { isJSON: true })
-          }</script>`
-        )
-      }
-      firstChunk = false
-    }
-    res.write(chunk)
-  })
-
-  renderStream.on('end', () => {
-    res.end(html.tail)
-    console.log(`whole request: ${Date.now() - s}ms`)
-  })
-
-  renderStream.on('error', err => {
-    throw err
-  })
-})
-
-const port = process.env.PORT || 8080
-app.listen(port, () => {
-  console.log(`server started at localhost:${port}`)
-})
