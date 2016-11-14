bc01eed6ff9629962df02024817c88e169bc5771
Addy Osmani <addyosmani@gmail.com>
2016-11-13 17:32:38 -0800

    Update api-client to modular Firebase

--- src/store/create-api-client.js
+++ src/store/create-api-client.js
@@ -1,3 +1,11 @@
-import Firebase from 'firebase'
+import Firebase from 'firebase/app'
+import 'firebase/database'
 
-export default new Firebase('https://hacker-news.firebaseio.com/v0')
+const config = {
+  databaseURL: 'https://hacker-news.firebaseio.com'
+}
+const version = '/v0'
+
+Firebase.initializeApp(config)
+const api = Firebase.database().ref(version)
+export default api
