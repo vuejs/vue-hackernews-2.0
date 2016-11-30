096e7f5c4562e9e169ba68b8d0321ba8f9a9fd7b
Evan You <yyx990803@gmail.com>
2016-11-30 09:59:07 -0500

    bump deps

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -4,7 +4,7 @@
     <span class="title">
       <template v-if="item.url">
         <a :href="item.url" target="_blank">{{ item.title }}</a>
-        <span class="host">({{ item.url | host }})</span>
+        <span class="host"> ({{ item.url | host }})</span>
       </template>
       <template v-else>
         <router-link :to="'/item/' + item.id">{{ item.title }}</router-link>
