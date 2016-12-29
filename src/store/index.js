ade06c60c0c3db9b5df16be7765667d88b23a1be
Evan You <yyx990803@gmail.com>
2016-12-29 13:18:58 -0500

    limit client store item cache to 3 minutes

--- src/store/index.js
+++ src/store/index.js
@@ -36,6 +36,19 @@ const store = new Vuex.Store({
     },
 
     FETCH_ITEMS: ({ commit, state }, { ids }) => {
+      // on the client, the store itself serves as a cache.
+      // only fetch items that we do not already have, or has expired (3 minutes)
+      const now = Date.now()
+      ids = ids.filter(id => {
+        const item = state.items[id]
+        if (!item) {
+          return true
+        }
+        if (now - item.__lastUpdated > 1000 * 60 * 3) {
+          return true
+        }
+        return false
+      })
       if (ids.length) {
         return fetchItems(ids).then(items => commit('SET_ITEMS', { items }))
       } else {
