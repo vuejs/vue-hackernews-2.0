c76173c1efc33edfe5309840e8066ce3b0ed46f5
Evan You <yyx990803@gmail.com>
2017-03-16 10:46:47 +0800

    only hydrate on client if initial state is available

--- src/entry-client.js
+++ src/entry-client.js
@@ -3,7 +3,9 @@ import { app, store, router } from './app'
 
 // prime the store with server-initialized state.
 // the state is determined during SSR and inlined in the page markup.
-store.replaceState(window.__INITIAL_STATE__)
+if (window.__INITIAL_STATE__) {
+  store.replaceState(window.__INITIAL_STATE__)
+}
 
 // wait until router has resolved all async before hooks
 // and async components...
