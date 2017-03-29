996266e4e2e2a997e2b3b28cbbc6e3da6fbfd60c
Evan You <yyx990803@gmail.com>
2017-03-29 11:29:34 +0800

    use explicit basedir

--- server.js
+++ server.js
@@ -16,10 +16,13 @@ const app = express()
 function createRenderer (bundle, options) {
   // https://github.com/vuejs/vue/blob/dev/packages/vue-server-renderer/README.md#why-use-bundlerenderer
   return createBundleRenderer(bundle, Object.assign(options, {
+    // for component caching
     cache: require('lru-cache')({
       max: 1000,
       maxAge: 1000 * 60 * 15
-    })
+    }),
+    // this is only needed when vue-server-renderer is npm-linked
+    basedir: resolve('./dist'),
   }))
 }
 
