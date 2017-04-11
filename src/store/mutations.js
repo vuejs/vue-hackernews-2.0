530a486c5d3bbf351116d136d3e1ab8030955acb
Evan You <yyx990803@gmail.com>
2017-04-11 11:07:11 +0800

    restructure

---
+++ src/store/mutations.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+
+export default {
+  SET_ACTIVE_TYPE: (state, { type }) => {
+    state.activeType = type
+  },
+
+  SET_LIST: (state, { type, ids }) => {
+    state.lists[type] = ids
+  },
+
+  SET_ITEMS: (state, { items }) => {
+    items.forEach(item => {
+      if (item) {
+        Vue.set(state.items, item.id, item)
+      }
+    })
+  },
+
+  SET_USER: (state, { user }) => {
+    Vue.set(state.users, user.id, user)
+  }
+}
--- src/views/CreateListView.js
+++ src/views/CreateListView.js
@@ -3,7 +3,7 @@ import ItemList from '../components/ItemList.vue'
 // This is a factory function for dynamically creating root-level list views,
 // since they share most of the logic except for the type of items to display.
 // They are essentially higher order components wrapping ItemList.vue.
-export function createListView (type) {
+export default function createListView (type) {
   return {
     name: `${type}-stories-view`,
     // this will be called during SSR to pre-fetch data into the store!
