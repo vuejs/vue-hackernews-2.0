49645e49d77db50bfc96e0eb6b10d930ecc2c002
Evan You <yyx990803@gmail.com>
2017-04-13 15:06:29 +0800

    logRequests

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 // this is aliased in webpack config based on server/client build
 import { createAPI } from 'create-api'
 
+const logRequests = !!process.env.DEBUG_API
+
 const api = createAPI({
   version: '/v0',
   config: {
@@ -20,8 +22,10 @@ function warmCache () {
 }
 
 function fetch (child) {
+  logRequests && console.log(`fetching ${child}...`)
   const cache = api.cachedItems
   if (cache && cache.has(child)) {
+    logRequests && console.log(`cache hit for ${child}.`)
     return Promise.resolve(cache.get(child))
   } else {
     return new Promise((resolve, reject) => {
@@ -30,6 +34,7 @@ function fetch (child) {
         // mark the timestamp when this item is cached
         if (val) val.__lastUpdated = Date.now()
         cache && cache.set(child, val)
+        logRequests && console.log(`fetched ${child}.`)
         resolve(val)
       }, reject)
     })
