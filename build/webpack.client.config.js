8d2acab2934876259a230bccbb36ec24d3db7bad
Evan You <yyx990803@gmail.com>
2017-04-16 21:53:18 +0800

    update deps and config

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge')
 const base = require('./webpack.base.config')
 const vueConfig = require('./vue-loader.config')
 const SWPrecachePlugin = require('sw-precache-webpack-plugin')
-const { VueSSRClientPlugin } = require('vue-ssr-webpack-plugin')
+const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
 
 const config = merge(base, {
   entry: './src/entry-client.js',
--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const base = require('./webpack.base.config')
-const { VueSSRServerPlugin } = require('vue-ssr-webpack-plugin')
+const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
 
 module.exports = merge(base, {
   target: 'node',
