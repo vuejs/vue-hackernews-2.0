4537fdd66701c5a4d617d5567d95f7a48ef0dfc9
Evan You <yyx990803@gmail.com>
2017-05-12 10:32:19 +0800

    use dynamic import instead of System.import

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,9 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 // route-level code splitting
-const createListView = id => () => System.import('../views/CreateListView').then(m => m.default(id))
-const ItemView = () => System.import('../views/ItemView.vue')
-const UserView = () => System.import('../views/UserView.vue')
+const createListView = id => () => import('../views/CreateListView').then(m => m.default(id))
+const ItemView = () => import('../views/ItemView.vue')
+const UserView = () => import('../views/UserView.vue')
 
 export function createRouter () {
   return new Router({
