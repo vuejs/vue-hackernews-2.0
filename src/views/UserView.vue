0a05d814d58150afdae74fbd504ce1e0c1dcf021
Tang Weizhi <tangwz.com@gmail.com>
2017-06-02 15:19:40 +0800

    add some spaces.

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -4,7 +4,7 @@
       <h1>User : {{ user.id }}</h1>
       <ul class="meta">
         <li><span class="label">Created:</span> {{ user.created | timeAgo }} ago</li>
-        <li><span class="label">Karma:</span> {{user.karma}}</li>
+        <li><span class="label">Karma:</span> {{ user.karma }}</li>
         <li v-if="user.about" v-html="user.about" class="about"></li>
       </ul>
       <p class="links">
