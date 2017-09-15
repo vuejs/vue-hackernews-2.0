5af138f33bfb6b99e343aba4a14b8131dc1f9c7e
JounQin <admin@1stg.me>
2017-09-16 00:20:17 +0800

    1. fix issue related to `route-cache` 2. use `this.$route` directly in component method 3. bump dependencies

--- src/entry-server.js
+++ src/entry-server.js
@@ -13,7 +13,7 @@ export default context => {
     const { app, router, store } = createApp()
 
     const { url } = context
-    const fullPath = router.resolve(url).route.fullPath
+    const { fullPath } = router.resolve(url).route
 
     if (fullPath !== url) {
       reject({ url: fullPath })
