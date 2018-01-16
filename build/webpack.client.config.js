56011f63d799e0148a4a2b4b2567a5b9f5b515f5
Flower Db <dearestdyw@gmail.com>
2018-01-16 15:56:37 +0800

    fix typo

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -4,6 +4,7 @@ const base = require('./webpack.base.config')
 const SWPrecachePlugin = require('sw-precache-webpack-plugin')
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
 
 const config = merge(base, {
   entry: {
