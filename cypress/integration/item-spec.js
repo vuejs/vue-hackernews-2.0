d9de141eeaec051113d317ff674e7a31085b131e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-23 23:39:35 -0500

    working filters on Item tests except for router-link

--- cypress/integration/item-spec.js
+++ cypress/integration/item-spec.js
@@ -1,25 +1,43 @@
 import Item from '../../src/components/Item.vue'
-import { timeAgo } from '../../src/util/filters'
-import Vue from 'vue'
+import { timeAgo, host } from '../../src/util/filters'
 
 const mountVue = require('cypress-vue-unit-test')
 /* eslint-env mocha */
-/* global cy */
-describe.skip('Item', () => {
-  const template = `<news-item item="item"></news-item>`
+/* global cy, Cypress */
+describe('Item', () => {
+  const template = `<news-item :item="item"></news-item>`
   const components = {
-    'news-item': Item,
-    'router-link': Vue.component('router-link', {
-      name: 'router-link'
-    })
+    'news-item': Item
   }
   const data = {
-    item: {}
+    item: {
+      title: 'Vue unit testing with Cypress',
+      score: 101,
+      url: 'https://www.cypress.io',
+      id: 'a0x',
+      by: 'bahmutov',
+      time: Cypress.moment('Jan 22 2018').unix(),
+      descendants: 42
+    }
+  }
+  const extensions = {
+    filters: { timeAgo, host }
   }
   const options = {
-    vue: 'https://unpkg.com/vue'
+    vue: 'https://unpkg.com/vue',
+    extensions
   }
+
+  beforeEach(() => {
+    cy.viewport(400, 200)
+  })
   beforeEach(mountVue({ template, components, data }, options))
 
-  it('loads', () => {})
+  it('loads news item', () => {
+    cy.contains('.score', 101)
+  })
+
+  it('has link to comments', () => {
+    cy.contains('router-link', '42 comments')
+  })
 })
