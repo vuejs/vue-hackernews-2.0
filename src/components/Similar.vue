b04651b0bd5b898206774339f26d7d547a04d200
Christoffer Åhrling <christoffer@nilpath.se>
2020-03-20 14:21:50 +0100

    redesign similar posts

--- src/components/Similar.vue
+++ src/components/Similar.vue
@@ -4,6 +4,7 @@
     <span class="title">Similar: </span>
     <ul class="list" v-if="story.similar && story.similar.length !== 0">
       <li  v-for="sim in story.similar" :key="sim.id">
+        <span class="box" v-bind:style="{ background: `rgba(255, 165, 0, ${sim.score})`}" v-bind:title="sim.score"></span>
         <router-link :to="'/item/' + sim.id">{{ sim.text }}</router-link>
       </li>
     </ul>
@@ -26,6 +27,12 @@ export default {
   .similar-posts
     line-height 1.2
 
+    .box
+      width: 12px
+      height: .85em
+      display: inline-block
+      margin-right: 8px
+
     .title
       font-size .85em
     ul.list, .no-posts
@@ -34,9 +41,11 @@ export default {
       margin 4px 0
       padding 8px 12px
       border-radius 4px
-      list-style circle inside
+      li
+        padding: 4px
+        display: flex
+        align-items: center
     a
       color: #828282;
-      text-decoration: underline;
 
 </style>
