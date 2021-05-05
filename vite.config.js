14f6f1879b43f8610add60342661bf915f5c4b20
Evan You <yyx990803@gmail.com>
2021-05-05 12:29:57 -0400

    migration: switch to Vue 3 migration build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2'
+import createVuePlugin from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [createVuePlugin()]
+  resolve: {
+    alias: {
+      vue: '@vue/compat'
+    }
+  },
+  plugins: [
+    createVuePlugin({
+      template: {
+        compilerOptions: {
+          compatConfig: {
+            MODE: 2
+          }
+        }
+      }
+    })
+  ]
 })
