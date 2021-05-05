a6e0c9ac7b1f4131908a4b1e43641f608593f714
Evan You <yyx990803@gmail.com>
2021-05-05 18:03:46 -0400

    migration: update global mounting API usage
    https://v3.vuejs.org/guide/migration/global-api.html#a-new-global-api-createapp

---
+++ src/main.js
@@ -0,0 +1,68 @@
+import { createApp } from 'vue'
+import { createStore } from './store'
+import { createRouter } from './router'
+import { sync } from 'vuex-router-sync'
+import titleMixin from './util/title'
+import App from './App.vue'
+import ProgressBar from './components/ProgressBar.vue'
+
+const store = createStore()
+const router = createRouter()
+
+sync(store, router)
+
+const app = createApp({
+  router,
+  store,
+  ...App
+})
+
+// global progress bar
+const bar = createApp(ProgressBar).mount('#progress-container')
+document.body.appendChild(bar.$el)
+app.config.globalProperties.$bar = bar
+
+// mixin for handling title
+app.mixin(titleMixin)
+
+// a global mixin that calls `asyncData` when a route component's params change
+app.mixin({
+  beforeRouteUpdate(to, from, next) {
+    const { asyncData } = this.$options
+    if (asyncData) {
+      asyncData({
+        store: this.$store,
+        route: to
+      })
+        .then(next)
+        .catch(next)
+    } else {
+      next()
+    }
+  }
+})
+
+// Add router hook for handling asyncData.
+router.beforeResolve((to, from, next) => {
+  const matched = router.getMatchedComponents(to)
+  const prevMatched = router.getMatchedComponents(from)
+  let diffed = false
+  const activated = matched.filter((c, i) => {
+    return diffed || (diffed = prevMatched[i] !== c)
+  })
+  const asyncDataHooks = activated.map((c) => c.asyncData).filter((_) => _)
+  if (!asyncDataHooks.length) {
+    return next()
+  }
+
+  bar.start()
+  Promise.all(asyncDataHooks.map((hook) => hook({ store, route: to })))
+    .then(() => {
+      bar.finish()
+      next()
+    })
+    .catch(next)
+})
+
+// actually mount to DOM
+app.mount('#app')
